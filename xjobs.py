4bf22d6f1a7a9832c52ccfa8e7e1d1c971cc944c
Wenjie Zhang <xerrors@163.com>
2023-10-04 13:47:40 +0800

    update

--- xjobs.py
+++ xjobs.py
@@ -12,25 +12,23 @@ hostname = os.getenv("HOSTNAME", "Kirin")
 def run():
     # Configure Runner
     runner = Runner(
-        name=hostname + "-B512",
+        name=hostname + "-C512",
         configuation_index="./configuation_dict.yaml",
         block_configuation=block_configuation,
     )
 
     # Add arguments
-    runner.add(dataset_config="datasets/ace2004/ace2004.yaml", use_rel_ner="no_mask",  seed=[43, 44, 45, 46])
-    runner.add(dataset_config="datasets/ace2005/ace2005.yaml", use_rel_ner="no_mask",  seed=[43, 44, 45, 46])
-    runner.add(dataset_config="datasets/ace2005/ace2005.yaml", seed=[43, 44, 45, 46])
+    runner.add(dataset_config="datasets/ace2004/ace2004.yaml", use_rel_ner="no_mask",  seed=[42, 43, 44, 45, 46])
+    runner.add(dataset_config="datasets/ace2005/ace2005.yaml", use_rel_ner="no_mask",  seed=[42, 43, 44, 45, 46])
 
     # Add tests
     runner.add_test(
         use_thres_val=True,
         test_opt1=["best"],
         use_thres_threshold=[0.0005, 0.0001, 0.00005],
-        test_from_ckpt=["output/ouput-2023-10-04_09-59-31-Oxen-B512","output/ouput-2023-10-04_08-52-29-Oxen-B512","output/ouput-2023-10-04_07-45-22-Oxen-B512","output/ouput-2023-10-04_06-38-16-Oxen-B512","output/ouput-2023-10-04_05-31-21-Oxen-B512","output/ouput-2023-10-04_04-23-06-Oxen-B512","output/ouput-2023-10-04_03-15-35-Oxen-B512","output/ouput-2023-10-04_02-07-53-Oxen-B512","output/ouput-2023-10-04_01-00-11-Oxen-B512","output/ouput-2023-10-03_23-52-18-Oxen-B512"],
+        test_from_ckpt=[],
         test_batch_size=1,
-        offline=True,
-        )
+        offline=True)
 
     runner.run(main, sort_by_seed=True)
 
